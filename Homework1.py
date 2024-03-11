520df7d616e198406a39cf4f2516c08d2a05a0d1
Arkadii <104249783+m0skaRAD@users.noreply.github.com>
2024-03-11 23:00:34 +0300

    Homework1.py

---
+++ Homework1.py
@@ -0,0 +1,55 @@
+#a = int(input("Введіть число - "))
+#if a % 2 == 0:
+#    print("Число кратне 2")
+#else:
+#    print("Число не кратнке 2")
+
+
+
+#b = int(input("Введіть число - ")) 
+#c = 1
+#while (c <= b):
+#    print(c)
+#    c += 1
+
+
+
+#d = int(input("Введіть число - "))
+#n = 2
+#if d % n != 0 + n < 10:
+#    print ("складне число")
+#    n += 1
+#else:
+#    print("просте число")
+#    n += 1
+
+
+
+#e = 0
+#for i in range(1, 101):
+#    e += i
+#print(e)
+
+
+
+#def check():
+#    g = int(input())
+#    h = int(input())
+#    if g < h:
+#        while (g <= h):
+#            print(g)
+#            g += 1
+#    elif h < g:
+#        while (h <= g):
+#            print(h)
+#            h += 1
+#check()
+
+
+n = int(input())
+f = 1
+while (n > 1):
+    f *= n
+    n -= 1
+ 
+print(f)
